6ede9faf090a3d67ba9a43049acc80ddc70504d8
GyuHyeon Hwang <57509844+hgyuhyeon@users.noreply.github.com>
2024-07-15 17:30:41 +0900

    #4 - Update: 카드2 / 황규현 / 07.15 / 228ms

--- Baekjoon/2164-카드2/2164-카드2-황규현.java
+++ Baekjoon/2164-카드2/2164-카드2-황규현.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
 		int n = sc.nextInt();
 		Queue<Integer> q = new LinkedList<>();
 		for (int i = 1; i <= n; i++) q.add(i);
