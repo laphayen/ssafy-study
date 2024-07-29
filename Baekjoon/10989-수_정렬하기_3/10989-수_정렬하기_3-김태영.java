c63b5a5f89c019d83a61e2aa63a846a25a9f9b2b
taehaha <ktylp0728@gmail.com>
2024-07-29 22:43:49 +0900

    #20 - Add: 수 정렬하기 3 / 김태영 / 07.29 / 2688ms

---
+++ Baekjoon/10989-수_정렬하기_3/10989-수_정렬하기_3-김태영.java
@@ -0,0 +1,27 @@
+mport java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < N; i++) {
+            bw.write(Integer.toString(arr[i]));
+            bw.newLine();
+        }
+        bw.close();
+    }
+}
